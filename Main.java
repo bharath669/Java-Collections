b0a2ab777811a6b67569375100c44d856cced446
“bharath669” <vvbharath2006@gmail.com>
2025-12-14 14:09:49 +0530

    Using Vector with Generics and methods

--- Main.java
+++ Main.java
@@ -5,11 +5,27 @@ import java.util.Vector;
 
 class Main{
     public static void main(String[] args){
-        Vector v=new Vector<>();
+        Vector<Integer> v=new Vector<Integer>();
         v.add(23);
         v.add(45);
         v.add(56);
-        System.out.println(v.capacity());
-        //if we add 10 elements and the capacity is ncrease to 20
+        v.add(89);
+        v.add(47);
+        v.add(36);
+        v.add(65);
+        v.add(93);
+        v.add(23);
+        v.add(21);
+        v.add(34);
+        v.add(78);
+        v.add(42);
+        v.add(78);
+        v.add(67);
+
+        v.remove(1);
+        System.out.println("capacity of vector is:"+v.capacity());
+        for(int i:v){
+            System.out.println(i);
+        }
     }
 }
