5dbe30ade4bb6b382b9948af68f663e95883c00a
“bharath669” <vvbharath2006@gmail.com>
2025-12-14 13:55:43 +0530

    Using Vector with Capacity()

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 
 class Main{
     public static void main(String[] args){
-        List<Integer>values=new ArrayList<>();
-        values.add(13);
-        values.add(23);
-        values.add(77);
-        values.add(45);
-        Collections.sort(values);
-
-        for(int i:values){
-            System.out.println(i);
-        } 
+        Vector v=new Vector<>();
+        v.add(23);
+        v.add(45);
+        v.add(56);
+        System.out.println(v.capacity());
+        //if we add 10 elements and the capacity is ncrease to 20
     }
 }
